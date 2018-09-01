c402fb0f8f71b30694b830bfdf4aabfaa762b1eb
Sabrina <selee734@gmail.com>
2018-08-31 18:03:23 -0700

    SabrinaOp added servo

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/SabrinaOp.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/SabrinaOp.java
@@ -15,10 +15,12 @@ import com.qualcomm.robotcore.util.Range;
 @Disabled
 public class SabrinaOp extends OpMode {
     //Declare any motors, servos, and sensors
-
+    Servo clawArm; //180
 
     //Declare any variables & constants pertaining to specific robot mechanisms (i.e. drive train)
-
+    final double CLAW_ARM_START_POS = 0.2;
+    double clawArmPosition= CLAW_ARM_START_POS;
+    double clawDelta =0.01;
 
     public SabrinaOp() {}
 
@@ -48,6 +50,8 @@ public class SabrinaOp extends OpMode {
 
     }
 
+
+
     void initialization() {
         //Clip and Initialize Specific Robot Mechanisms
 
@@ -66,6 +70,17 @@ public class SabrinaOp extends OpMode {
          //Step 2: (Physical Instructions on how to control specific robot mechanism using controller buttons)
          //Step ...: (Physical Instructions on how to control specific robot mechanism using controller buttons)
   */
+
+    //driver 1
+    //step 1: press the up/down button to move claw towards 180/0
+    void updateClaw () {
+        if (gamepad1.dpad_up) {
+          clawArmPosition+=clawDelta;
+        }
+        else if (gamepad1.dpad_down){
+            clawArmPosition-=clawDelta;
+        }
+    }
 
 
     //Create variables/methods that will be used in ALL autonomous programs for this specific robot
